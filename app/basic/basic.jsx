71277bbd04c2add60b8ea402f9bb5cbe93740e56
グエン マイン テウン <nguyenmanhtung93@gmail.com>
2018-04-10 13:51:14 +0700

    [RRA-001] Fix app/basic/basic.jsx using React.Component class in ES6

--- app/basic/basic.jsx
+++ app/basic/basic.jsx
@@ -5,26 +5,28 @@ import Home from './home.jsx';
 import About from './about.jsx';
 import Topics from './topics.jsx';
 
-export default () => (
-  <HashRouter>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-      </ul>
-
-      <hr />
-
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
-    </div>
-  </HashRouter>
-);
+export default class Basic extends React.Component {
+    render() {
+        return (
+            <HashRouter>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/topics">Topics</Link>
+                        </li>
+                    </ul>
+                    <hr/>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/about" component={About}/>
+                    <Route path="/topics" component={Topics}/>
+                </div>
+            </HashRouter>
+        );
+    }
+}
